bbcb8418f31a30191aee25ca91da65115279a1ea
Harsh Sharma <harsh@codelif.in>
2026-04-27 13:03:35 +0530

    fix: update token endpoint

--- src/wrapper.js
+++ src/wrapper.js
@@ -195,7 +195,7 @@ export class WebPortal {
    */
   async student_login(username, password, captcha = DEFCAPTCHA) {
     let pretoken_endpoint = "/token/pretoken-check";
-    let token_endpoint = "/token/generatetoken";
+    let token_endpoint = "/token/generatewebtoken";
 
     let payload = { username: username, usertype: "S", captcha: captcha };
     payload = await serialize_payload(payload);
